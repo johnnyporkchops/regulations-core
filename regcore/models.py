01eb43c94c68d4999520c3c86df76e5e9cc0e37b
John Carroll <jcarroll@macadmins-MacBook-Pro-4.local>
2021-11-06 11:38:48 -0400

    revert migration 003 try set with trailing comma for index togethor

--- regcore/models.py
+++ regcore/models.py
@@ -18,7 +18,7 @@ class Document(MPTTModel):
     root = models.BooleanField(default=False, db_index=True)
 
     class Meta:
-        index_together = (('doc_type', 'version', 'label_string'),)
+        index_together = [['doc_type', 'version', 'label_string'],]
         # index_together = normalize_together(index_together)
         unique_together = (('doc_type', 'version', 'label_string'),)
         # unique_together = normalize_together(unique_together)
