20503840977161f7db2512ed680f1d2467ec3df2
John Carroll <jcarroll@macadmins-MacBook-Pro-4.local>
2021-11-06 11:56:48 -0400

    normalize_togethor uncomment mistake

--- regcore/models.py
+++ regcore/models.py
@@ -19,7 +19,7 @@ class Document(MPTTModel):
 
     class Meta:
 
-        index_together = (('doc_type', 'version', 'label_string'),)
+        index_together = {{'doc_type', 'version', 'label_string'},}
         index_together = normalize_together(index_together)
         unique_together = (('doc_type', 'version', 'label_string'),)
         #unique_together = normalize_together(unique_together)
